41e156217f00a5b8cea81d0d836181fccdbc607e
jvcbyte <jvc8463@gmail.com>
2025-08-28 15:50:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rock of Ages Auto Technology',
+  webDir: 'dist'
+};
+
+export default config;
