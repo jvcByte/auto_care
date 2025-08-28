33fce807651ea99cb4c4381635305f279b65055e
jvcbyte <jvc8463@gmail.com>
2025-08-28 16:23:11 +0100

    chore: rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Rock of Ages Auto Technology',
+  appName: 'RoA Auto Care',
   webDir: 'dist'
 };
 
