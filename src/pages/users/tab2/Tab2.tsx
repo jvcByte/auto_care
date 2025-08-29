34c8171571fda49a6b356c8a1594410e390df807
jvcbyte <jvc8463@gmail.com>
2025-08-29 16:19:46 +0100

    fix: use double ionHeader to avoid encroching status mobile status bar component for other tabs

--- src/pages/users/tab2/Tab2.tsx
+++ src/pages/users/tab2/Tab2.tsx
@@ -1,13 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../../components/ExploreContainer/ExploreContainer';
 import './Tab2.css';
+import { cameraOutline, ellipsisHorizontalOutline, ellipsisVerticalOutline } from 'ionicons/icons';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader className='ion-no-border'>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle></IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonHeader translucent={true} className='ion-no-border'>
+        <IonToolbar>
+          <IonButtons slot='secondary'>
+            <IonButton>
+              <IonIcon icon={cameraOutline} />
+            </IonButton>
+            <IonButton>
+              <IonIcon slot='icon-only' ios={ellipsisHorizontalOutline} md={ellipsisVerticalOutline} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Services</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +30,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Services page" />
       </IonContent>
     </IonPage>
   );
--- src/pages/users/tab3/Tab3.tsx
+++ src/pages/users/tab3/Tab3.tsx
@@ -1,13 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../../components/ExploreContainer/ExploreContainer';
 import './Tab3.css';
+import { cameraOutline, ellipsisHorizontalOutline, ellipsisVerticalOutline } from 'ionicons/icons';
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader className='ion-no-border'>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle></IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonHeader translucent={true} className='ion-no-border'>
+        <IonToolbar>
+          <IonButtons slot='secondary'>
+            <IonButton>
+              <IonIcon icon={cameraOutline} />
+            </IonButton>
+            <IonButton>
+              <IonIcon slot='icon-only' ios={ellipsisHorizontalOutline} md={ellipsisVerticalOutline} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +30,7 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name="Profile page" />
       </IonContent>
     </IonPage>
   );
