ae49a514bc79970073e12791ac549bcb44bd0045
jvcbyte <jvc8463@gmail.com>
2025-08-29 18:25:51 +0100

    refactor(users): Tab1, Tab2, Tab3, renamed

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import { Route, Switch } from 'react-router-dom';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Welcome from './components/Welcome/Welcome';
 
 setupIonicReact();
