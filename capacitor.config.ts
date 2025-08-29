fd018da1eb36717f2b66596961a6b3ff83a3c9a3
jvcbyte <jvc8463@gmail.com>
2025-08-29 16:15:00 +0100

    fix: use double ionHeader to avoid encroching status mobile status bar component

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'RoA Auto Care',
+  appName: 'AutoCare',
   webDir: 'dist'
 };
 
